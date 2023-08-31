0470f5989961b623767c7c6bc4c0350599a89359
haykal-fe-verd <haykal@verd.net.id>
2023-08-31 21:22:49 +0700

    fix issues

--- resources/js/layouts/AuthLayout.jsx
+++ resources/js/layouts/AuthLayout.jsx
@@ -32,10 +32,10 @@ function AuthLayout({ children }) {
 
     return (
         <div className="relative h-full">
-            <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80]">
+            <div className="hidden h-full lg:flex lg:w-72 lg:flex-col lg:fixed lg:inset-y-0 z-[80]">
                 <Sidebar />
             </div>
-            <main className="md:pl-72">
+            <main className="lg:pl-72">
                 <Topbar />
                 <div className="m-5">{children}</div>
             </main>
